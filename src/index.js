14f898b07e4502eb26c25b75e94d3470223116a7
AMEXX <b123013@iiit-bh.ac.in>
2025-09-04 14:32:43 +0000

    database connection and debugging

---
+++ src/index.js
@@ -0,0 +1,34 @@
+
+// require('dotenv').config({path : './env'})
+
+import connectDB from "./db/index.js";
+import dotenv from "dotenv"
+
+dotenv.config({
+  path: './env'
+})
+
+
+
+connectDB()
+
+// const app = express();
+
+// (async () => {
+//   try {
+//     await  mongoose.connect(`${process.env.MONGODB_URL}/ ${DB_NAME}`);
+//     app.on("error", (error) => {
+//       console.log("the app is not able to talk to the database", error);
+//       throw error;
+//     });
+//     app.listen(process.env.PORT, () => {
+//       console.log("The server is talking to the database");
+//     });
+//   } catch (error) {
+//     console.log(
+//       `there was some problem in connecting with database on port ${process.env.PORT}`
+//     );
+//     throw error;
+//   }
+// })();
+// pwir
