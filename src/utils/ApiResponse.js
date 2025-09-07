2f46304e0290d3e4d04ad5271a3afb461536cbff
AMEXX <b123013@iiit-bh.ac.in>
2025-09-07 14:26:15 +0000

    controllers

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,1 +1,8 @@
- 
+ class ApiResponse {
+    constructor(statusCode, data, message = "Success") {
+        this.statusCode = statusCode
+        this.message = message
+        this.data = data
+        this.success = statusCode < 400 // should be less than 400 because anything above it will send through the error response
+    }
+}
